b5515235fd391b27a1fb5465b78ac8c9487786f3
Uzair-Ali-Bhatti <uzairalibhatti03@gmail.com>
2025-10-31 10:20:52 +0500

    Update task.py

--- task.py
+++ task.py
@@ -1,14 +1,17 @@
+#file handling with text file
 #create a tast
 #view a task 
 #exet program
+# append_example.py
 
-
-print("=========================")
-print("Please Select one option")
-print("1: Add new tak")
-print("2: view task")
-print("0: Exet")
-print("=========================")
+def menu():
+    print("=========================")
+    print("Please Select one option")
+    print("1: Add new tak")
+    print("2: view task")
+    print("3: Delete task")
+    print("0: Exit")
+    print("=========================")
 
 tasks = []
 
@@ -22,15 +25,22 @@ def show_tasks():
             print(i, t)
     else:
         print("No task Avalable")
+def del_task():
+    user_input = int(input("Enter task no. you want to delete: "))
+    tasks.pop(user_input -1)
+    print(f"{user_input} is deleted successfully")
     
     
 while(True):
     try:
+        menu()
         user_input = int(input("=>"))
         if user_input == 1:
             add_task(tasks)
         elif user_input == 2:
             show_tasks()
+        elif user_input == 3:
+            del_task()
         elif user_input == 0:
             break
         else:
